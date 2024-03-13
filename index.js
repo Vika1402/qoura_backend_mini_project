f9d453dc94149789cf167e9cad8c35da5c08b791
vikas <vikas.kumar123@ssipmt.com>
2024-03-13 07:11:32 +0530

    introduce uuid package in project for unique id each post

--- index.js
+++ index.js
@@ -3,64 +3,59 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-
+const { v4: uuidv4 } = require("uuid");
 app.set("view engine", "ejs");
-app.set("views",path.join(__dirname,"views"));
-app.use(express.static(path.join(__dirname,"public")));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-let posts=[
+let posts = [
   {
-    id:"1a",
-    username:"vikas kumar",
-    content:"I love coding ",
-
+    id: uuidv4(),
+    username: "vikas kumar",
+    content: "I love coding ",
   },
   {
-    id:"2b",
-    username:"mehata ji ",
-    content:"tark mehta ka ulta chasma ",
-    
+    id: uuidv4(),
+    username: "mehata ji ",
+    content: "tark mehta ka ulta chasma ",
   },
   {
-    id:"3c",
-    username:"jetha lal",
-    content:"I love gujrat ",
-    
+    id: uuidv4(),
+    username: "jetha lal",
+    content: "I love gujrat ",
   },
   {
-    id:"4d",
-    username:"bidhe uncle ",
-    content:"i am marathi man ",
-    
-  }
+    id: uuidv4(),
+    username: "bidhe uncle ",
+    content: "i am marathi man ",
+  },
 ];
 
-// see all post in a page 
-app.get("/posts",(req,res)=>{
-  res.render("index.ejs",{posts})
-})
-//to add new post using form page 
-app.get("/posts/new",(req,res)=>{
-  res.render("new.ejs",{posts})
-})
-// after submit is redirest and show all post 
-app.post("/posts",(req,res)=>{
-  let {username,content}=req.body;
-  posts.push({username,content})
-  res.redirect("/posts")
-})
+// see all post in a page
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+//to add new post using form page
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs", { posts });
+});
+// after submit is redirest and show all post
+app.post("/posts", (req, res) => {
+  let id=uuidv4();
+  let { username, content } = req.body;
+  posts.push({id, username, content });
+  res.redirect("/posts");
+});
 
-app.get("/posts/:id",(req,res)=>{
-  let {id}=req.params;
-  let post=posts.find((p)=>id===p.id)
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
   console.log(id);
-  res.render("show.ejs",{post})
-})
-
+  res.render("show.ejs", { post });
+});
 
 app.listen(port, (req, res) => {
   console.log("listing port number", port);
